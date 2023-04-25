2ef3c546101f6559b9f34b974cc34fab99c9462a
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-04-25 18:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	var fname=document.getElementById("f").value;
+	var lname=document.getElementById("l").value;
+	alert(fname+lname);
+	
 
 }
+var button =document.getElementById("button")
+button.addEventListener('click', getFormvalue)
+
+
+
